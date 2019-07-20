a54bc911322af5d10503d1b6a0021c04f2ec656b
Calum Sieppert <sieppertcalum@gmail.com>
2019-07-20 12:16:20 -0600

    README update and cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,18 @@ import React, { useEffect, useRef, useState } from 'react'
 
 import robinImage from '../../static/robin.png'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-
 export default function IndexPage() {
-  const [ml5Loaded, setMl5Loaded]           = useState(false)
+  const [ml5Loaded, setMl5Loaded] = useState(false)
   const [loadingResults, setLoadingResults] = useState(false)
-  const [results, setResults]               = useState()
+  const [results, setResults] = useState()
 
   const robinImageRef = useRef()
-
-  if (ml5Loaded && !loadingResults) {
-    (async function() {
-        setLoadingResults(true)
-        const classifier = await ml5.imageClassifier('MobileNet')
-
-        const results = await classifier.classify(robinImageRef.current)
-
-        setResults(results)
-        setLoadingResults(false)
-      }
-    )()
-  }
 
   useEffect(() => {
     const script = document.createElement('script')
 
-    script.src    = 'https://unpkg.com/ml5@0.3.1/dist/ml5.min.js'
-    script.async  = true
+    script.src = 'https://unpkg.com/ml5@0.3.1/dist/ml5.min.js'
+    script.async = true
     script.onload = () => {
       setMl5Loaded(true)
     }
@@ -37,15 +21,31 @@ export default function IndexPage() {
     document.body.appendChild(script)
   }, [])
 
+  useEffect(() => {
+    if (ml5Loaded && !loadingResults) {
+      ;(async function() {
+        setLoadingResults(true)
+        const classifier = await ml5.imageClassifier('MobileNet')
+
+        const results = await classifier.classify(robinImageRef.current)
+
+        setResults(results)
+        setLoadingResults(false)
+      })()
+    }
+  }, [ml5Loaded])
+
   return (
-    <Layout>
-      <SEO title='Home' />
+    <>
       <h1>ML5 demo</h1>
       <img src={robinImage} alt='robin' ref={robinImageRef} />
       {loadingResults && <p>ML5 is guessing...</p>}
-      {results &&
-       <p>ML5 guess: {results[0].label} <br />ML5
-         confidence: {results[0].confidence}</p>}
-    </Layout>
+      {results && (
+        <p>
+          ML5 guess: {results[0].label} <br />
+          ML5 confidence: {results[0].confidence}
+        </p>
+      )}
+    </>
   )
 }
